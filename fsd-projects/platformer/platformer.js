3968f595170a769f207f7dc6c4a660b617a5e1a7
Caubrey7 <carmanaubreytran7@gmail.com>
2026-09-03 14:13:10 +0000

    Added Platform

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(255, 180, 242)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,24 +33,35 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    
 
     // TODO 2 - Create Platforms
-
-
-
+    //createPlatform(x, y, width, height, "color")
+    createPlatform(300, 700, 100, 10, "white");
+    createPlatform(400, 600, 100, 10, "white");
+    createPlatform(500, 500, 100, 10, "white");
+    createPlatform(300, 400, 100, 10, "white");
+    createPlatform(200, 300, 100, 10, "white");
+    createPlatform(400, 200, 100, 10, "white");
+    createPlatform(600, 200, 100, 10, "white");
+    createPlatform(800, 300, 100, 10, "white");
+    createPlatform(1000, 400, 100, 10, "white");
+    createPlatform(900, 500, 100, 10, "white");
+    createPlatform(600, 200, 20, 600, "white");
+    createPlatform(800, 600, 100, 10, "white")
 
     // TODO 3 - Create Collectables
+    //createCollectable(x, y, gravity, bounce)
+    createCollectable("diamond", 200, 170, 0.5, 0.7);
+    createCollectable("max", 800, 200, 0.5, 0.7);
+    createCollectable("steve", 700, 500, 0.5, 0.7);
 
-
-
-    
     // TODO 4 - Create Cannons
+    //createCannon(side, position, delay)
+    createCannon("right", 300, 4000)
+    createCannon("right", 500, 5000)
+    createCannon("right", 700, 6000)
 
-
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
